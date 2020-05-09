3fdf637cfabd108e6ecfff442516e33f2ac667d7
felipetempus <felipemouradev@gmail.com>
2020-05-09 14:17:32 -0400

    done

---
+++ chaining.js
@@ -0,0 +1,3 @@
+const integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
+
+console.log(integers.sort((a, b) => a - b).filter(e => e < 19).map(e => e * 1.5 - 1).reduce((former, newer) => former + newer))
